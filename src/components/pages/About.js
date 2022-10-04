25fd88f6c502b641e3f1464df8dcbcedeb0d21d5
Mirvaliyev <mirvaliyev2000@gmail.com>
2022-10-04 16:09:11 +0500

    change

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div className='w-full h-full flex justify-center items-center'>
      <h1 className=' md:text-[200px] text-[80px] font-bold text-[#222]'>
-      Abouttt
+      About
      </h1>
     </div>
   )
