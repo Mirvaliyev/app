8e9d591a007d8fee0814db150bebbd9e93e70c72
Mirvaliyev <mirvaliyev2000@gmail.com>
2022-10-04 16:57:07 +0500

    change

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ function App() {
   return (
     <div className=' flex justify-self-stretch'>
       <BrowserRouter>
-        <nav className='z-50 nav bg-amber-100 md:shadow-[20px_0px_30px_rgba(0,0,0,0.3)] shadow-[0px_-15px_30px_rgba(0,0,0,0.2)] border-t-[1px] md:border-t-[0px] md:border-r-[1px] border-gray-400 fixed flex items-center md:block bottom-0 w-full h-[60px] md:left-0 md:top-0 md:h-screen md:w-[130px]'>
+        <nav className='z-50 nav bg-amber-100 md:shadow-[20px_0px_30px_rgba(0,0,0,0.3)] shadow-[0px_-15px_30px_rgba(0,0,0,0.2)] border-t-[1px] md:border-t-[0px] md:border-r-[1px] border-gray-500 fixed flex items-center md:block bottom-0 w-full h-[60px] md:left-0 md:top-0 md:h-screen md:w-[130px] overflow-y-scroll'>
           <Navbar />
         </nav>
-        <main id='main' className='main fixed top-0 w-full h-[calc(100vh-60px)] md:right-0 md:top-0 md:h-screen md:w-[calc(100vw-130px)] p-[30px] bg-amber-100'>
+        <main id='main' className='main fixed top-0 w-full h-[calc(100vh-60px)] md:right-0 md:top-0 md:h-screen md:w-[calc(100vw-130px)] bg-amber-100 overflow-y-scroll'>
           <Routes>
             <Route exact path='/' element={<Home />} />
             <Route exact path='/about' element={<About />} />
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,8 +2,8 @@ import React from 'react'
 import my_logo from '../../icons/my_logo.svg'
 export default function Home() {
   return (
-    <div className="w-full h-full home">
-      <img className=' absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[calc(250px+5vmin)] drop-shadow-[0px_20px_30px_rgba(0,0,0,0.8)]' src={my_logo} />
+    <div className="w-full h-screen min-h-[100vh] home flex justify-center overflow-y-scroll border-[2px] border-red-0 p-[60px]">
+      <img className='w-[calc(250px+5vmin)] drop-shadow-[0px_20px_30px_rgba(0,0,0,0.8)]' src={my_logo} />
     </div>
       )
 }
